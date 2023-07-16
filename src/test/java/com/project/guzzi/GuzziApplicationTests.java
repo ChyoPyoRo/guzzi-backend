9523df2a7dbe18601b7e21b976f750329c22ab9b
hyeonjeong4031 <hjn40300@gmail.com>
2023-07-16 16:44:37 +0900

    intellij

---
+++ src/test/java/com/project/guzzi/GuzziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.guzzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuzziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
