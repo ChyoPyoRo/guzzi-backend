6fd5a9f95e3ec1b4df574974cd0bb052fa8e244f
ChyoPyoRo <allen15590@naver.com>
2023-07-28 21:08:41 +0900

    dependency 해결

--- src/test/java/com/project/guzzi/GuzziApplicationTests.java
+++ src/test/java/com/project/guzzi/GuzziApplicationTests.java
@@ -3,7 +3,7 @@ package com.project.guzzi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GuzziApplicationTests.class)
 class GuzziApplicationTests {
 
 	@Test
